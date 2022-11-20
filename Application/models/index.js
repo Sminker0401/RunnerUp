88f694f236ade54eac1e267e5360a5fd76b3842a
Tim Mardis <mardistj96@yahoo.com>
2022-11-20 00:34:37 -0600

    added models and server.js

---
+++ Application/models/index.js
@@ -0,0 +1,13 @@
+const User = require('./User');
+const Entry = require('./Entry');
+
+User.hasMany(Entry, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Entry.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Entry };
