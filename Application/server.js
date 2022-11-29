574d54deb5117bb02bfafe10a6dc0216466d36cc
Tim Mardis <mardistj96@yahoo.com>
2022-11-29 17:29:48 -0600

    Added handlebars functionality

--- Application/server.js
+++ Application/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const sequelize = require('./config/connection');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -12,6 +13,8 @@ const routes = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({});
 
 const sess = {
   secret: 'Super secret secret',
@@ -29,6 +32,9 @@ const sess = {
 };
 
 app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
