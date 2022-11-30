7babaa3e5708e5e743858ee27ea8fcf125fbe7d0
Tim Mardis <mardistj96@yahoo.com>
2022-11-29 19:28:35 -0600

    Entry is posted to database

--- Application/public/js/login.js
+++ Application/public/js/login.js
@@ -1,5 +1,26 @@
 const loginButton = document.getElementById('login-button');
 
-loginButton.addEventListener('click', function() {
-    document.location.replace('/homepage')
+// loginButton.addEventListener('click', function(event) {
+//     event.preventDefault();
+//     document.location.replace('/homepage')
+// })
+
+loginButton.addEventListener('click', async function (event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
+
+    const response = await fetch('/api/users/', {
+      method: 'POST',
+      body: JSON.stringify({ username, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/homepage');
+    } else {
+        alert(response.statusText);
+    }
+
 })
